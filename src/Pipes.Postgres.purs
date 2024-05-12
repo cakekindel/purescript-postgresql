27a7abb32952b447aa52e7bfade7a74a8df8200b
Orion Kindel <orionkindel@gmail.com>
2024-05-11 23:00:05 -0500

    fix: stdin txn

--- src/Pipes.Postgres.purs
+++ src/Pipes.Postgres.purs
@@ -2,7 +2,7 @@ module Pipes.Postgres where
 
 import Prelude
 
-import Control.Monad.Morph (hoist)
+import Control.Monad.Error.Class (class MonadError, class MonadThrow, catchError, throwError)
 import Control.Monad.Postgres (class MonadPostgres)
 import Control.Monad.Reader (class MonadAsk, ask)
 import Data.Maybe (Maybe(..))
@@ -11,6 +11,7 @@ import Effect.Aff.Postgres.Client as Client
 import Effect.Aff.Postgres.Pool (Pool)
 import Effect.Aff.Postgres.Pool as Pool
 import Effect.Class (liftEffect)
+import Effect.Exception (Error)
 import Node.Buffer (Buffer)
 import Node.Stream.Object as O
 import Pipes ((>->))
@@ -21,6 +22,7 @@ import Pipes.Prelude as Pipes
 stdin
   :: forall m s c ct
    . MonadAff m
+  => MonadError Error m
   => MonadAsk Pool m
   => MonadPostgres m s c ct
   => String
@@ -29,16 +31,26 @@ stdin q = do
   pool <- ask
   client <- liftAff $ Pool.connect pool
   stream <- liftEffect $ Client.execWithStdin q client
+  liftAff $ void $ Client.exec "begin" client
   let
-    releaseOnEOS Nothing = Pool.release pool client $> Nothing
+    releaseOnEOS Nothing = do
+      liftAff $ void $ Client.exec "commit" client
+      liftEffect $ Pool.release pool client
+      pure Nothing
     releaseOnEOS (Just a) = pure (Just a)
-  hoist liftAff
-    $ hoist liftEffect (Pipes.mapM releaseOnEOS)
-        >-> fromWritable (O.fromBufferWritable stream)
+
+    pipe = Pipes.mapM releaseOnEOS >-> fromWritable (O.fromBufferWritable stream)
+    err e = do
+       liftAff $ void $ Client.exec "rollback" client
+       liftEffect $ Pool.release pool client
+       throwError e
+
+  catchError pipe err
 
 stdout
   :: forall m s c ct
    . MonadAff m
+  => MonadThrow Error m
   => MonadAsk Pool m
   => MonadPostgres m s c ct
   => String
@@ -48,8 +60,6 @@ stdout q = do
   client <- liftAff $ Pool.connect pool
   stream <- liftEffect $ Client.queryWithStdout q client
   let
-    releaseOnEOS Nothing = Pool.release pool client $> Nothing
+    releaseOnEOS Nothing = liftEffect $ Pool.release pool client $> Nothing
     releaseOnEOS (Just a) = pure (Just a)
-  hoist liftAff
-    $ fromReadable (O.fromBufferReadable stream)
-        >-> hoist liftEffect (Pipes.mapM releaseOnEOS)
+  fromReadable (O.fromBufferReadable stream) >-> Pipes.mapM releaseOnEOS
